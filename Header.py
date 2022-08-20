210106d875e8ce281a51a43599bf1cdced3960db
Marco Kittel <marco@planhero.de>
2022-08-20 17:05:35 +0200

    new

--- Header.py
+++ Header.py
@@ -17,9 +17,10 @@ def youtube(my_canvas,x=0,y=0):
     my_canvas.drawImage('youtbe.png', 1300+x, 0+200+y, width=200, preserveAspectRatio=True, mask='auto')
     #my_canvas.drawString(1165+90+15+60+x, 200+60+y,'Video von mir zur PDF Erzeugung')
     my_canvas.setFont('Helvetica', 20)
-    my_canvas.drawString(1345+90+15+40+x, 200+65+y,'https://youtu.be/18QtztXJ-mc')
-    my_canvas.drawString(1345+90+15+40+x, 200+40+y,'https://youtu.be/_9EFi-L2HmI')
-    my_canvas.drawString(1345+90+15+40+x, 200+15+y,'https://youtu.be/bnTPVeZwaC4')
+   #my_canvas.drawString(1345+90+15+40+x, 200+65+y,'https://youtu.be/18QtztXJ-mc')
+   #my_canvas.drawString(1345+90+15+40+x, 200+40+y,'https://youtu.be/_9EFi-L2HmI')
+   #my_canvas.drawString(1345+90+15+40+x, 200+15+y,'https://youtu.be/bnTPVeZwaC4')
+    my_canvas.drawString(1345+90+15+40+x, 200+15+y,'https://youtu.be/LHupmqn8vYg')
 def getHeader():
     packet = io.BytesIO()
     width = 2143
